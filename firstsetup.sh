4318fab05a130af71763cc76fdb02821cfb14da5
jjgit-hub <jjgit-hub@skintag.org>
2021-12-17 02:05:46 -0600

    reorder and fix openssl to use sudo

--- firstsetup.sh
+++ firstsetup.sh
@@ -1,3 +1,10 @@
+# setup mailserver
+docker-compose run mailserver setup email add it-admin@example.com
+docker-compose run mailserver setup email list
+docker-compose run mailserver setup alias add postmaster@example.com it-admin
+docker-compose run mailserver setup alias list
+docker-compose run mailserver setup config dkim
+
 # setup traefik letsencrypt
 sudo mkdir -p ./docker-data/traefik/
 sudo touch ./docker-data/traefik/acme.json
@@ -6,13 +13,7 @@ sudo chmod 600 ./docker-data/traefik/acme.json
 # encrypt mail
 # https://doc.dovecot.org/configuration_manual/mail_crypt_plugin/#ec-key
 sudo mkdir -p ./docker-data/dms/certs/
-openssl ecparam -list_curves
-openssl ecparam -name prime256v1 -genkey | openssl pkey -out ./docker-data/dms/certs/ecprivkey.pem
-openssl pkey -in ecprivkey.pem -pubout -out ./docker-data/dms/certs/ecpubkey.pem
+sudo openssl ecparam -list_curves
+sudo openssl ecparam -name prime256v1 -genkey | openssl pkey -out ./docker-data/dms/certs/ecprivkey.pem
+sudo openssl pkey -in ecprivkey.pem -pubout -out ./docker-data/dms/certs/ecpubkey.pem
 
-# setup mailserver
-docker-compose run mailserver setup email add it-admin@example.com
-docker-compose run mailserver setup email list
-docker-compose run mailserver setup alias add postmaster@example.com it-admin
-docker-compose run mailserver setup alias list
-docker-compose run mailserver setup config dkim
