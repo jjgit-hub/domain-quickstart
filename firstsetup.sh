e8ce414f7f2c48c5783091d9d8ddfb7deaa5abfd
jjgit-hub <jjgit-hub@skintag.org>
2021-12-17 02:31:16 -0600

    cleanup

--- firstsetup.sh
+++ firstsetup.sh
@@ -6,7 +6,6 @@ sudo chmod 600 ./docker-data/traefik/acme.json
 # encrypt mail
 # https://doc.dovecot.org/configuration_manual/mail_crypt_plugin/#ec-key
 sudo mkdir -p ./docker-data/dms/certs/
-sudo openssl ecparam -list_curves
 sudo openssl ecparam -name prime256v1 -genkey | sudo openssl pkey -out ./docker-data/dms/certs/ecprivkey.pem
 sudo openssl pkey -in ./docker-data/dms/certs/ecprivkey.pem -pubout -out ./docker-data/dms/certs/ecpubkey.pem
 
