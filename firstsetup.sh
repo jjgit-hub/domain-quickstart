dc2e7fc8c1ed335166f01e958e80a0bc91730a92
jjgit-hub <jjgit-hub@skintag.org>
2022-01-15 16:03:55 -0600

    switch to volumes

--- firstsetup.sh
+++ firstsetup.sh
@@ -1,13 +1,11 @@
-# setup traefik letsencrypt
-sudo mkdir -p ./docker-data/traefik/
-sudo touch ./docker-data/traefik/acme.json
-sudo chmod 600 ./docker-data/traefik/acme.json
-
 # encrypt mail
 # https://doc.dovecot.org/configuration_manual/mail_crypt_plugin/#ec-key
-sudo mkdir -p ./docker-data/dms/certs/
-sudo openssl ecparam -name prime256v1 -genkey | sudo openssl pkey -out ./docker-data/dms/certs/ecprivkey.pem
-sudo openssl pkey -in ./docker-data/dms/certs/ecprivkey.pem -pubout -out ./docker-data/dms/certs/ecpubkey.pem
+docker volume create dms-config-custom-volume
+docker cp /host_dir mycontainer:/container_dir
+docker volume create website-volume
+docker volume create ngnix-config-volume
+docker-compose run mailserver openssl ecparam -name prime256v1 -genkey | openssl pkey -out /certs/ecprivkey.pem
+docker-compose run mailserver openssl pkey -in /certs/ecprivkey.pem -pubout -out /certs/ecpubkey.pem
 
 # setup mailserver
 echo "creating it-admin@johnston.host"
@@ -18,5 +16,5 @@ docker-compose run mailserver setup alias list
 docker-compose run mailserver setup config dkim
 
 docker-compose up -d
-sudo cat ./docker-data/dms/config/opendkim/keys/johnston.host/mail.txt
+docker-compose run mailserver cat /tmp/docker-mailserver/opendkim/keys/johnston.host/mail.txt
 docker-compose logs -f
